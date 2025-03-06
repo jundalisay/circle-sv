df45602c4d10339aaf89a12d5f9cb5f86b1abc0b
“jundalisay” <jundalisay@yahoo.com>
2025-03-06 14:15:15 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -13,3 +13,25 @@ const config = {
 };
 
 export default config;
+
+
+
+// import adapter from '@sveltejs/adapter-cloudflare';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			// See below for an explanation of these options
+// 			routes: {
+// 				include: ['/*'],
+// 				exclude: ['<all>']
+// 			},
+// 			platformProxy: {
+// 				configPath: undefined,
+// 				environment: undefined,
+// 				persist: undefined
+// 			}
+// 		})
+// 	}
+// };
